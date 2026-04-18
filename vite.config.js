adddf6ece0ead9e6efcba3b282a871069263a859
subburavi6-glitch <subburavi6@gmail.com>
2026-04-18 18:26:16 +0530

    Remove subfolder base, host at root

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/civicbid/',
-  plugins: [react()],
+plugins: [react()],
   optimizeDeps: {
     include: ['pdfjs-dist'],
   },
